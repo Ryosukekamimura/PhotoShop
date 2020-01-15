4bc2e5b6b3d255c169da6d2db8ca199aad9ccccf
Ryosuke Kamimura <come1234560@gmail.com>
2020-01-15 14:12:44 +0900

    神村亮佑という文字を追加しました。

--- PhotoShop/PhotoShop/MainWindow.xaml.cs
+++ PhotoShop/PhotoShop/MainWindow.xaml.cs
@@ -20,10 +20,7 @@ namespace PhotoShop
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
