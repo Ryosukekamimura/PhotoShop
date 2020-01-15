92bedfc7988249650c82f016b88a67433a6371f3
Ryosuke Kamimura <come1234560@gmail.com>
2020-01-15 14:53:14 +0900

    PhotoShopUIほぼ完成

---
+++ PhotoShop/PhotoShop/GaussianFilter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoShop
+{
+    class GaussianFilter
+    {
+        public static byte[,] LoadImageGray(string filename)
+        {
+            Bitmap img = new Bitmap(filename);
+            int w = img.Width;
+            int h = img.Height;
+            byte[,] dst = new byte[w, h];
+
+            //bitmapクラスの画像ピクセル値を配列に挿入
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    dst[j, i] = (byte)((img.GetPixel(j, i).R + img.GetPixel(j, i).G + img.GetPixel(j, i).B) / 3);
+
+
+                }
+            }
+            return dst;
+        }
+
+        public static byte[,] Filter(byte[,] src, double[,] kernel)
+        {
+            int w = src.GetLength(0);
+            int h = src.GetLength(1);
+
+            int kernelSize = kernel.GetLength(0);
+
+            //出力画像用の配列
+            byte[,] dst = new byte[w, h];
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    double sum = 0;
+                    for (int k = -kernelSize / 2; k <= kernelSize / 2; k++)
+                    {
+                        for (int n = -kernelSize / 2; n <= kernelSize / 2; n++)
+                        {
+                            if (j + n >= 0 && j + n < w && i + k >= 0 && i + k < h)
+                            {
+                                sum += src[j + n, i + k] * kernel[n + kernelSize / 2, k + kernelSize / 2];
+
+                            }
+                        }
+                    }
+                    dst[j, i] = Byte2Int(sum);
+
+                }
+
+
+            }
+            return dst;
+        }
+        public static void SaveImagr(byte[,] src, string filename)
+        {
+            int w = src.GetLength(0);
+            int h = src.GetLength(1);
+            Bitmap img = new Bitmap(w, h);
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    img.SetPixel(j, i, Color.FromArgb(src[j, i], src[j, i], src[j, i]));
+
+                }
+            }
+            img.Save(filename);
+            Console.WriteLine("完了しました。");
+
+
+        }
+
+        public static byte Byte2Int(double num)
+        {
+            if (num > 255.0) return 255;
+            else if (num < 0) return 0;
+            else return (byte)num;
+
+        }
+
+
+
+    }
+}
--- PhotoShop/PhotoShop/MainWindow.xaml.cs
+++ PhotoShop/PhotoShop/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,10 +21,128 @@ namespace PhotoShop
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
-        public MainWindow() => InitializeComponent();
+        public string ImageSourcePath = null;
+        public int first_image_skip = 0;
 
 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void open_button_Click(object sender, RoutedEventArgs e)
+        {
+            //最初はタブが生成されているのでスキップする。
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImageSourcePath = openFileDialog.FileName;
+
+            }
+            BitmapImage bitmapImage = new BitmapImage(new Uri(ImageSourcePath));
+
+            if (first_image_skip == 0)
+            {
+                imageBox01.Source = bitmapImage;
+                first_image_skip++;
+            }
+            else
+            {
+                TabItem tabItem = new TabItem()
+                {
+                    Header = "Test",
+                    Name = "test",
+                };
+                tabControl.Items.Add(tabItem);
+
+            }
+
+
+
+
+        }
+
+        private void attribute_Click(object sender, RoutedEventArgs e)
+        {
+            SubAttributeWindow sAW = new SubAttributeWindow();
+            sAW.Owner = this;
+            sAW.Left = sAW.Owner.Left + sAW.Owner.Width - sAW.Width;
+            sAW.Top = sAW.Owner.Top + sAW.Owner.Height - sAW.Height;
+
+            BitmapImage bitmapImage = new BitmapImage(new Uri(ImageSourcePath));
+            sAW.documentProperty_Width.Text = bitmapImage.PixelWidth.ToString();
+            sAW.documentProperty_Height.Text = bitmapImage.PixelHeight.ToString();
+
+
+
+
+
+            sAW.Show();
+        }
+
+        private void attribute_DragOver(object sender, DragEventArgs e)
+        {
+            SubAttributeWindow sAW = new SubAttributeWindow();
+            sAW.Owner = this;
+            sAW.Show();
+        }
+
+        private void attribute_Drop(object sender, DragEventArgs e)
+        {
+            SubAttributeWindow sAW = new SubAttributeWindow();
+            sAW.Owner = this;
+            sAW.Show();
+        }
+
+        private void gray_button_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            using (Mat mat = new Mat(ImageSourcePath))
+            using (Mat matGray = mat.CvtColor(ColorConversionCodes.BGR2GRAY))
+            {
+                //Cv2.ImShow("grayscale_show", matGray);
+                //imageBox01.Source = matGray;
+                BitmapSource bitmapSource = BitmapSourceConverter.ToBitmapSource(matGray);
+                imageBox01.Source = bitmapSource;
+            }
+        }
+
+        private void Gaussian_filter_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            byte[,] img = GaussianFilter.LoadImageGray(ImageSourcePath);
+            const int kernelSize = 3;
+            double[,] kernel = new double[kernelSize, kernelSize]
+            {
+                {1/16.0, 1/8.0,1/16.0 },
+                {1/8.0, 1/4.0,1/8.0 },
+                {1/16.0, 1/8.0,1/16.0 },
+
+            };
+            byte[,] img2 = GaussianFilter.Filter(img, kernel);
+
+
+
+            //画像保存
+            GaussianFilter.SaveImagr(img2, "dst.jpg");
+
+        }
+
+        private void test_Click(object sender, RoutedEventArgs e)
+        {
+            //ここはテスト画面です。
+            //他のウィンドウに移動し、テストしてください。
+            Test testWindow = new Test();
+            testWindow.Show();
+        }
+
+        private void test1_Click(object sender, RoutedEventArgs e)
+        {
+            //ここはテスト用xamlです。
+            Test1 test1 = new Test1();
+            test1.Show();
+        }
     }
 }
